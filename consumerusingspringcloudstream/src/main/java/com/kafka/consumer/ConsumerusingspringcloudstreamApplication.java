6eb84113bb3ac59ea726e669b76b52d472c4a070
Akash Wadageri <wadagireakash92@gmail.com>
2025-08-15 23:22:44 +0530

    Final updated code for the kafka

---
+++ consumerusingspringcloudstream/src/main/java/com/kafka/consumer/ConsumerusingspringcloudstreamApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerusingspringcloudstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerusingspringcloudstreamApplication.class, args);
+	}
+
+}
